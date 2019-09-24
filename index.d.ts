489e7c18865a4b32f61608a28a47f501533fb4cb
star <502545703@qq.com>
2019-09-25 00:12:05 +0800

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+// Type definitions
+
+interface Options {
+  logToFile?: boolean;
+  dir?: string;
+  byDay?: boolean;
+}
+
+declare class Logger {
+  /**
+   * Options:
+   *   - `logToFile` optional, Whether to write to a file, default:false
+   *   - `dir` optional, log folder location, default: './log'
+   *   - `byDay` optional, Whether to cut by day, default: false
+   */
+  constructor(options: Options);
+
+  /**
+   * print info message 
+   * 
+   *  `message` The message to write  
+   */
+  info(message: string): void;
+
+  /**
+   * print warn message 
+   * 
+   *  `message` The message to write  
+   */
+  warn(message: string): void;
+
+  /**
+   * print error message 
+   * 
+   *  `message` The message to write  
+   */
+  error(message: string): void;
+}
+
+export = Logger
