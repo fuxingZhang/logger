9a24d3304888e7797f384873d0a6adb01f06438d
star <502545703@qq.com>
2019-09-25 14:42:14 +0800

    add jsdoc

--- stdout.js
+++ stdout.js
@@ -1,6 +1,11 @@
 const isTTY = process.stdout.isTTY;
 const eol = require('os').EOL;  // \n on POSIX, \r\n on Windows
 
+/**
+ * print to terminal
+ * 
+ * @param {String} chunk data to write
+ */
 function stdout(chunk) {
   process.stdout.write(`${chunk}${eol}`);
 }
