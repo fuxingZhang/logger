37a13d79f84d8ebf2381bb9437591b0e7dc02348
star <502545703@qq.com>
2020-02-23 14:00:51 +0800

    add five methods

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const colors = require('@zhangfuxing/colors/fn');
 const path = require('path');
 const write = require('./write');
 const Dater = require('@zhangfuxing/date');
-let stdout = require('./stdout');
+const stdout = require('./stdout');
 const eol = require('os').EOL;
 const noop = () => void 0;
 
 class Logger {
+  #stdout;
+  #rotate;
+  #dir;
+  #info = this.info;
+  #warn = this.warn;
+  #error = this.error;
+  #write = Logger.write;
+
   /**
    * When isTTY is false, logger will not write to terminal.
    * 
@@ -18,12 +26,23 @@ class Logger {
    * @param {Boolean} rotate optional, Whether rotate logs by day, default: false
    * @param {Boolean} disableConsole optional, disable write to terminal, default: false
    */
-  constructor({ dir, rotate, disableConsole } = {}) {
-    if (disableConsole === true) stdout = noop;
-    this.rotate = rotate;
+  constructor({ dir, rotate, disableConsole = false } = {}) {
+    if (rotate !== undefined) {
+      if (typeof rotate !== 'boolean') {
+        throw new TypeError('rotate must be boolean');
+      }
+      this.#rotate = rotate;
+    }
+
+    if (disableConsole !== undefined && typeof disableConsole !== 'boolean') {
+      throw new TypeError('disableConsole must be boolean');
+    }
+    this.#stdout = disableConsole === true ? noop : stdout;
+
     if (dir !== undefined) {
-      this.dir = path.resolve(dir);
-      Logger.init(this.dir);
+      if (typeof dir !== 'string') throw new TypeError('dir must be string');
+      this.#dir = path.resolve(dir);
+      Logger.init(this.#dir, this.#stdout);
     }
   }
 
@@ -32,8 +51,9 @@ class Logger {
    * 
    * @private
    * @param {String} dir 
+   * @param {Function} stdout 
    */
-  static init(dir) {
+  static init(dir, stdout) {
     if (fs.existsSync(dir)) return;
     stdout(`${Logger.getWarn()} Log folder does not exist`);
     try {
@@ -51,13 +71,13 @@ class Logger {
    */
   info(...args) {
     const message = Logger.format(args);
-    stdout(`${Logger.getInfo()} ${message}`);
+    this.#stdout(`${Logger.getInfo()} ${message}`);
 
-    if (this.dir !== undefined) Logger.write({
-      dir: this.dir,
+    if (this.#dir !== undefined) Logger.write({
+      dir: this.#dir,
       type: 'info',
       message,
-      rotate: this.rotate
+      rotate: this.#rotate
     });
   }
 
@@ -68,13 +88,13 @@ class Logger {
    */
   warn(...args) {
     const message = Logger.format(args);
-    stdout(`${Logger.getWarn()} ${message}`);
+    this.#stdout(`${Logger.getWarn()} ${message}`);
 
-    if (this.dir !== undefined) Logger.write({
-      dir: this.dir,
+    if (this.#dir !== undefined) Logger.write({
+      dir: this.#dir,
       type: 'warn',
       message,
-      rotate: this.rotate
+      rotate: this.#rotate
     });
   }
 
@@ -85,13 +105,13 @@ class Logger {
    */
   error(...args) {
     const message = Logger.format(args);
-    stdout(`${Logger.getError()} ${message}`);
+    this.#stdout(`${Logger.getError()} ${message}`);
 
-    if (this.dir !== undefined) Logger.write({
-      dir: this.dir,
+    if (this.#dir !== undefined) Logger.write({
+      dir: this.#dir,
       type: 'error',
       message,
-      rotate: this.rotate
+      rotate: this.#rotate
     });
   }
 
@@ -112,20 +132,61 @@ class Logger {
   }
 
   /**
-   * disable write to terminal and file for unit testing
+   * disable  write to file and terminal
    * @public
    */
   disable() {
-    stdout = noop;
     this.info = noop;
     this.warn = noop;
     this.error = noop;
   }
 
+  /**
+   * restore previous log configuration: file, terminal or both
+   * @public
+   */
+  enable() {
+    this.info = this.#info;
+    this.warn = this.#warn;
+    this.error = this.#error;
+  }
+
+  /**
+   * disable write to terminal
+   * @public
+   */
+  disableConsole() {
+    this.#stdout = noop;
+  }
+
+  /**
+   * enable write to terminal
+   * @public
+   */
+  enableConsole() {
+    this.#stdout = stdout;
+  }
+
+  /**
+   * disable write to file
+   * @public
+   */
+  disableFile() {
+    Logger.write = noop;
+  }
+
+  /**
+   * enable write to file
+   * @public
+   */
+  enableFile() {
+    Logger.write = this.#write;
+  }
+
   static format(args) {
     args = args.map(arg => {
-      if(arg instanceof Error) return arg.stack;
-      if(Object.prototype.toString.call(arg) === '[object Object]') return JSON.stringify(arg, null, 2);
+      if (arg instanceof Error) return arg.stack;
+      if (Object.prototype.toString.call(arg) === '[object Object]') return JSON.stringify(arg, null, 2);
       return arg;
     });
 
